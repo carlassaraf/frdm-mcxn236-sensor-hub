3abd512a4274b8608ef03dc0a5a53c65ae69f9f3
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-07-24 10:37:36 -0300

    test(lvgl): add a simple screen rotation test with switch

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/app_version.h>
+
+#include "ui.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -14,6 +18,30 @@ int main(void) {
   int ret = display_blanking_off(display);
   LOG_INF("blanking_off: %d", ret);
 
-  while(1);
+  const struct gpio_dt_spec btn = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
+  if(!gpio_is_ready_dt(&btn)) {
+    LOG_ERR("SW2 not ready");
+  }
+  if(gpio_pin_configure_dt(&btn, GPIO_INPUT)) {
+    LOG_ERR("SW2 not configured");
+  }
+
+  ui_init();
+
+  lv_obj_t *screens[] = {ui_scrSplash, ui_scrOverview, ui_scrTilt, ui_scrEnvironment, ui_scrCan};
+  uint8_t i = 0;
+
+  while (1) {
+    static bool pressed = false;
+    if(gpio_pin_get_dt(&btn) && !pressed) {
+      i = (i + 1) % (sizeof(screens) / sizeof(screens[0]));
+      _ui_screen_change(&(screens[i]), LV_SCR_LOAD_ANIM_NONE, 0, 0, NULL);
+      pressed = true;
+    } else if(!gpio_pin_get_dt(&btn) && pressed) {
+      pressed = false;
+    }
+    lv_timer_handler();
+    k_msleep(100);
+  }
   return 0;
 }
