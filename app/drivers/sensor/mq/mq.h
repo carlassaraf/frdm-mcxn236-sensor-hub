bcae6743fbd04c4aa96885ef0704ee9bf5b4f496
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2026-09-11 11:59:23 -0300

    feat(driver): add driver source code

---
+++ app/drivers/sensor/mq/mq.h
@@ -0,0 +1,21 @@
+#ifndef MQ_H
+#define MQ_H
+
+#include <zephyr/drivers/sensor.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum mq_channel {
+  SENSOR_CHAN_MQ_SMOKE = SENSOR_CHAN_PRIV_START,
+  SENSOR_CHAN_MQ_ALCOHOL,
+  SENSOR_CHAN_MQ_CO,
+  SENSOR_CHAN_MQ_ALARM,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
